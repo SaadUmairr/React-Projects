d1ca272e3575bdb770c364147fcfc61ed05bd149
Saad Umair <saad.ansari0106@gmail.com>
2023-10-13 17:05:41 +0530

    Add 04-Blog

---
+++ 04-blog/src/App.jsx
@@ -0,0 +1,13 @@
+import config from "./config/config";
+import "./App.css";
+
+function App() {
+    console.log(config.appwriteEndpoint);
+    return (
+        <>
+            <h1>SIMPLE BLOG </h1>
+        </>
+    );
+}
+
+export default App;
